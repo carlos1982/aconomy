4d95b2b17a1f17435223c858c5b23f8bc6159167
bison-- <mike.bison42@googlemail.com>
2014-06-08 23:29:26 +0200

    adding share events sql

---
+++ migrations/ShareEvents.sql
@@ -0,0 +1,17 @@
+
+--
+-- Tabellenstruktur für Tabelle `ShareEvents`
+--
+
+CREATE TABLE IF NOT EXISTS `ShareEvents` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `EncryptID` varchar(100) COLLATE utf8_bin NOT NULL,
+  `User` int(11) NOT NULL COMMENT 'Requester',
+  `Item` int(11) NOT NULL,
+  `Message` text COLLATE utf8_bin NOT NULL,
+  `MessageReturn` text COLLATE utf8_bin NOT NULL,
+  `Status` enum('Open','Rejected','Approved','HandedOver') COLLATE utf8_bin NOT NULL,
+  `LendFrom` datetime NOT NULL,
+  `LendTil` datetime NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
