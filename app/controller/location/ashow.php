6576c97fdf0e3dac18196dde970a430279ab7f9c
carlos1982 <cccastro@gmx.de>
2014-06-09 00:46:50 +0200

    Items einer Location ausgeben. Pre-Alpha.

--- app/controller/location/ashow.php
+++ app/controller/location/ashow.php
@@ -11,6 +11,19 @@ else { // Formal richtiger Aufruf
     $location->AddCondition('EncryptID', $token);
     if ($location->LoadFromDB()) {
         hDebug::Add('Location was found');
+
+        $location->LoadMemberships();
+        $member_ids = array();
+        foreach ($location->mMemberships->mItems as $member) {
+            $member_ids[] = $member->dUser->getValue();
+        }
+
+        $item_list = new lItems();
+        $item_list->AddCondition('User', $member_ids, 'in');
+        if ($item_list->LoadFromDB()) {
+            hStorage::addVar('ItemList', $item_list);
+        }
+
         hStorage::addVar('Location', $location);
     }
 }
